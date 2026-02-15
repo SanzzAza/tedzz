3cd86fd2207450f0e6ef9c830aee50335abd00cf
SanzzAza <harithemwan904@gmail.com>
2026-02-16 04:42:25 +0700

    Create drama.js

---
+++ api/drama.js
@@ -0,0 +1,11 @@
+const { getDramaList } = require('../lib/scraper');
+
+module.exports = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const data = await getDramaList(page);
+    res.json({ success: true, data });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
